762b09ed9408bd4e3ebe79a52cd7f0ed1f230b15
Metin Cavdar <MCavdar@rockwellautomation.com>
2021-05-20 22:59:38 +0300

    App setup for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = ''
 
     const location = search.value
-    const url = 'http://localhost:3000/weather?address=' + location
+    const url = '/weather?address=' + location
     fetch(url).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
